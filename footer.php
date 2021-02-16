7193dff1243c6e085e23f6dd7f0402071ca2ba4a
Skythel <bczcn@foxmail.com>
2021-02-16 23:06:29 +0800

    update

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
+    </div> <!-- End wrapper -->
+</body>
 <div class="footer">v<?php echo $app_ver; ?> ¤ Created by Heptagon</div>
 </html>
--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 $cfg_title = "Home"; 
 // Load standard header from file
 include 'header.php'; 
-?>
-<body>
-    <!-- Load standard dashboard module from file -->
-    <?php include 'navbar.php'; ?>
-    <!-- The wrapper div for all body content. -->
-    <div class="wrapper"> 
-        <?php // Load game file
-        include 'game.php'; ?>
-    </div> <!-- End wrapper -->
-</body>
-<?php // Load footer
+// Load game file
+include 'game.php'; 
+// Load footer
 include 'footer.php'; 
 ?>
