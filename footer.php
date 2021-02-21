8e3ad8f9995ed95156deee06353048a43e9a5267
Skythel <bczcn@foxmail.com>
2021-02-21 13:50:30 +0800

    corrected stray div

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
     </div> <!-- End wrapper -->
+    <div class="footer">v<?php echo $app_ver; ?> ¤ Created by Heptagon</div>
 </body>
-<div class="footer">v<?php echo $app_ver; ?> ¤ Created by Heptagon</div>
 </html>
