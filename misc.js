4ed8e1c8b3da9576d02b5c11abc14d70f51ba585
Skythel <bczcn@foxmail.com>
2021-03-06 01:36:17 +0800

    reposition messages

--- misc.js
+++ misc.js
@@ -2,16 +2,33 @@
 
 // Receives an error message from any other function and displays it in a user-friendly format. 
 function throwError(e) { 
+    clearMessages("all");
     var wrapper = document.getElementsByClassName("wrapper")[0];
     var element = document.createElement("div");
     element.classList.add("error");
+    element.classList.add("message");
     element.innerHTML = e;
     wrapper.appendChild(element);
 }
+// Receives a success message from any other function and displays it in a user-friendly format.
 function successMessage(m) {
+    clearMessages("all");
     var wrapper = document.getElementsByClassName("wrapper")[0];
     var element = document.createElement("div");
     element.classList.add("success");
+    element.classList.add("message");
     element.innerHTML = m;
     wrapper.appendChild(element);
+}
+// Clears all message elements of a certain type.
+function clearMessages(t) {
+    if(t=="all") {
+        var path = "document.getElementsByClassName(\"message\")";
+    }
+    else {
+        var path = "document.getElementsByClassName(t)";
+    }
+    for(var i=0; i<path.length; i++) {
+        path[i].parentNode.removeChild(path[i]);
+    }
 }
