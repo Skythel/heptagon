e445175db3460dfadcbad38f7acf9db6e5200553
hellomarshmellow <70008770+hellomarshmellow@users.noreply.github.com>
2021-03-17 04:24:03 +0800

    Delete heptagon-main directory

--- heptagon-main/footer.php
+++
@@ -1,5 +0,0 @@
-    </div> <!-- End wrapper -->
-    <div class="footer">v<?php echo $app_ver; ?> ¤ Created by Heptagon | <a href="https://github.com/Skythel/heptagon" target="_blank">source code</a></div>
-    <script src="misc.js"></script>
-</body>
-</html>
