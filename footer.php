feb025c371d02c6fe2c27d10f5c6d18de3216f44
Skythel <bczcn@foxmail.com>
2021-03-27 18:48:24 +0800

    debug

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
     </div> <!-- End wrapper -->
     <div class="footer">v<?php echo $app_ver; ?> ¤ Created by Heptagon | <a href="https://github.com/Skythel/heptagon" target="_blank">source code</a></div>
-    <script src="misc.js"></script>
+    <script src="misc.js?v=<?php echo $scripts_ver; ?>"></script>
 <?php if($cfg_title == "Login - MemoryMaze") { ?>
-    <script src="login.js"></script>
+    <script src="login.js?v=<?php echo $scripts_ver; ?>"></script>
 <?php } ?>
 </body>
 </html>
