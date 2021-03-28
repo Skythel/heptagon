0f0f29253ab8540cfae9333969805d26355856f0
Skythel <bczcn@foxmail.com>
2021-03-28 19:18:19 +0800

    add score things to profile

--- profile.php
+++ profile.php
@@ -29,33 +29,150 @@ else {
                 echo "<h1>Profile</h1><div class=\"error message\">Sorry, this user does not exist.</div>";
             }
             else {
-                $cfg_title = (isset($uid) ? $uid."'s " : "")."Profile - MemoryMaze"; ?>
-            
-                <!-- Content goes here -->
-                <h1><?php echo (isset($_SESSION["username"]) ? $_SESSION["username"]."'s " : "")."Profile"; ?></h1>
+                $cfg_title = (isset($uid) ? $uid."'s " : "")."Profile - MemoryMaze"; 
 
+                $diff = "easy";
+                $sql2 = $conn->prepare("SELECT `timestamp`,`time_taken`,`obstacles_hit`,`adjusted_score`,`hints_used`,`passcode_attempts` FROM `game_logs` WHERE `userid`=? AND `difficulty`=? GROUP BY `timestamp` ORDER BY `timestamp` DESC LIMIT 5");
+                if(
+                    $sql2 &&
+                    $sql2->bind_param("is",$uid,$diff) &&
+                    $sql2->execute() &&
+                    $sql2->store_result() &&
+                    $sql2->bind_result($easy_latest_timestamp,$easy_latest_timetaken,$easy_latest_obhit,$easy_latest_score,$easy_latest_hints,$easy_latest_passcodes)
+                ) {
+                    if($sql2->num_rows>0) {
+                        $easy_latest_output = "<table> 
+                            <tr>
+                                <th>Date Played</th>
+                                <th>Time Taken</th>
+                                <th>Obstacles Hit</th>
+                                <th>Hints Used</th>
+                                <th>Passcode Attempts</th>
+                                <th>Final Score</th>
+                            </tr>";
+                        while($sql2->fetch()) {
+                            $easy_latest_output .= "
+                            <tr>
+                                <td>".date("j M Y H:i",$easy_latest_timestamp)."</td>
+                                <td>".$easy_latest_timetaken." seconds</td>
+                                <td>".$easy_latest_obhit."</td>
+                                <td>".$easy_latest_hints."</td>
+                                <td>".$easy_latest_passcodes."</td>
+                                <td>".$easy_latest_score."</td>
+                            </tr>";
+                        }
+                        $easy_latest_output .= "</table>";
+                    }
+                    $sql2->close();
+                }
+                else {
+                    echo $conn->error;
+                }
+                
+                $diff = "medium";
+                $sql2 = $conn->prepare("SELECT `timestamp`,`time_taken`,`obstacles_hit`,`adjusted_score`,`hints_used`,`passcode_attempts` FROM `game_logs` WHERE `userid`=? AND `difficulty`=? GROUP BY `timestamp` ORDER BY `timestamp` DESC LIMIT 5");
+                if(
+                    $sql2 &&
+                    $sql2->bind_param("is",$uid,$diff) &&
+                    $sql2->execute() &&
+                    $sql2->store_result() &&
+                    $sql2->bind_result($medium_latest_timestamp,$medium_latest_timetaken,$medium_latest_obhit,$medium_latest_score,$medium_latest_hints,$medium_latest_passcodes)
+                ) {
+                    if($sql2->num_rows>0) {
+                        $medium_latest_output = "<table> 
+                            <tr>
+                                <th>Date Played</th>
+                                <th>Time Taken</th>
+                                <th>Obstacles Hit</th>
+                                <th>Hints Used</th>
+                                <th>Passcode Attempts</th>
+                                <th>Final Score</th>
+                            </tr>";
+                        while($sql2->fetch()) {
+                            $medium_latest_output .= "
+                            <tr>
+                                <td>".date("j M Y H:i",$medium_latest_timestamp)."</td>
+                                <td>".$medium_latest_timetaken." seconds</td>
+                                <td>".$medium_latest_obhit."</td>
+                                <td>".$medium_latest_hints."</td>
+                                <td>".$medium_latest_passcodes."</td>
+                                <td>".$medium_latest_score."</td>
+                            </tr>";
+                        }
+                        $medium_latest_output .= "</table>";
+                    }
+                    $sql2->close();
+                }
+                else {
+                    echo $conn->error;
+                }
+
+                $diff = "hard";
+                $sql2 = $conn->prepare("SELECT `timestamp`,`time_taken`,`obstacles_hit`,`adjusted_score` FROM `game_logs` WHERE `userid`=? AND `difficulty`=? GROUP BY `timestamp` ORDER BY `timestamp` DESC LIMIT 5");
+                if(
+                    $sql2 &&
+                    $sql2->bind_param("is",$uid,$diff) &&
+                    $sql2->execute() &&
+                    $sql2->store_result() &&
+                    $sql2->bind_result($hard_latest_timestamp,$hard_latest_timetaken,$hard_latest_obhit,$hard_latest_score)
+                ) {
+                    if($sql2->num_rows>0) {
+                        $hard_latest_output = "<table> 
+                            <tr>
+                                <th>Date Played</th>
+                                <th>Time Taken</th>
+                                <th>Obstacles Hit</th>
+                                <th>Hints Used</th>
+                                <th>Passcode Attempts</th>
+                                <th>Final Score</th>
+                            </tr>";
+                        while($sql2->fetch()) {
+                            $hard_latest_output .= "
+                            <tr>
+                                <td>".date("j M Y H:i",$hard_latest_timestamp)."</td>
+                                <td>".$hard_latest_timetaken." seconds</td>
+                                <td>".$hard_latest_obhit."</td>
+                                <td>".$hard_latest_hints."</td>
+                                <td>".$hard_latest_passcodes."</td>
+                                <td>".$hard_latest_score."</td>
+                            </tr>";
+                        }
+                        $hard_latest_output .= "</table>";
+                    }
+                    $sql2->close();
+                }
+                else {
+                    echo $conn->error;
+                }
+                ?>
+                <!-- Content goes here -->
                 <div class="profile-main">
                     <?php include 'sidebar.php'; ?>
-
+                    
                     <!--main container start-->
                     <div class="container-2">
-                        <div class="card">
-                            <h1>Username:</h1>
-                            <p>Username</p><br>
-                
-                            <h2>Email:</h2>
-                            <p>Username@gmail.com</p><br>
-                
-                            <h2>Highest Score:</h2>
-                            <p>73</p><br>
-                
-                            
-                        </div>
+                        <h1><?php echo (isset($_SESSION["username"]) ? $_SESSION["username"]."'s " : "")."Profile"; ?></h1>
+                        
+                        <?php
+                        if(isset($easy_latest_output)) {
+                            echo $easy_latest_output;
+                        }
+                        if(isset($medium_latest_output)) {
+                            echo $medium_latest_output;
+                        }
+                        if(isset($hard_latest_output)) {
+                            echo $hard_latest_output;
+                        }
+                        ?>
                     </div>
                     <!--main container end-->
                 </div>
                 <?php
             }
+            $sql->close();
+        }
+        else {
+            echo $conn->error;
         }
     } 
     else {
