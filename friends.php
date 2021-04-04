110d1d0874050edabc91e23a54b323996e548b31
Ky <14922694+Skythel@users.noreply.github.com>
2021-04-04 21:49:06 +0800

    Add files via upload

--- friends.php
+++ friends.php
@@ -95,11 +95,11 @@ if(isset($none) && $none) {
 
 <h2>Outgoing Friend Requests</h2>
 <?php 
-$sql = $conn->prepare("SELECT DISTINCT `friend_requests`.`timestamp`,`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`,
--- IFNULL(MAX(`logins`.`timestamp`),0)
+$sql = $conn->prepare("SELECT DISTINCT `friend_requests`.`timestamp`,`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`
+--, IFNULL(MAX(`logins`.`timestamp`),0)
 FROM `friend_requests`
 LEFT JOIN `users` ON `friend_requests`.`recipient_userid` = `users`.`userid`
-LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
+-- LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
 WHERE `friend_requests`.`sender_userid`=? AND `friend_requests`.`status`=0 LIMIT 10");
 if( 
     $sql &&
