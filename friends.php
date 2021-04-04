b73ae2df9c1a70edc297d5ab159ab03d67941d85
Ky <14922694+Skythel@users.noreply.github.com>
2021-04-04 21:59:33 +0800

    Add files via upload

--- friends.php
+++ friends.php
@@ -21,77 +21,113 @@ include 'header.php'; ?>
 
 <h2>Your Friends</h2>
 <?php 
-$sql = $conn->prepare("SELECT DISTINCT `friend_requests`.`timestamp`,`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`,IFNULL(MAX(`logins`.`timestamp`),0)
+$sql = $conn->prepare("SELECT DISTINCT -- `friend_requests`.`timestamp`,
+`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`
+-- ,IFNULL(MAX(`logins`.`timestamp`),0)
 FROM `friend_requests`
 LEFT JOIN `users` ON `friend_requests`.`recipient_userid` = `users`.`userid`
-LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
+-- LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
 WHERE (`friend_requests`.`sender_userid`=? OR `friend_requests`.`recipient_userid`=?) AND `friend_requests`.`status`=1 LIMIT 10");
 if(
     $sql &&
     $sql->bind_param('ii',$_SESSION["userid"],$_SESSION["userid"]) &&
     $sql->execute() &&
     $sql->store_result() &&
-    $sql->bind_result($request_timestamp,$recipient_id,$recipient_tag,$recipient_name,$recipient_regdate,$recipient_lastlogin)
+    $sql->bind_result(/*$request_timestamp,*/$recipient_id,$recipient_tag,$recipient_name,$recipient_regdate/*,$recipient_lastlogin*/)
 ) {
     if($sql->num_rows<1) {
         echo "Oops! It seems you don't have any friends yet. Why not add a user to your friend list?";
     }
     else {
-        $none = true;
+        // $none = true;
         while($sql->fetch()) {
-            if($recipient_lastlogin>0) {
-                $none = false;
-?>
-<div class="mini-profile">
-    <a href="./profile?u=<?php echo $recipient_id; ?>"><span class="mini-profile-name"><?php echo $recipient_name; ?>#<span class="mini-profile-tag"><?php echo $recipient_tag; ?></span></span></a><br/>
-    <!-- <img src="./assets/avocado.png" class="mini-profile-img" /><br/> 
-    <span class="mini-profile-high-score">High Score: 94</span><br/>-->
-    <span class="mini-profile-registered">Registered <?php echo date("j M Y",$recipient_regdate); ?></span><br/>
-    <span class="mini-profile-last-login">Last Active <?php echo date("j M Y",$recipient_lastlogin); ?></span><br/>
-    <span class="mini-profile-remove-friend" id="remove-friend-<?php echo $recipient_id; ?>" onclick="removeFriend(<?php echo $recipient_id; ?>)"><i class="fas fa-user-minus" aria-hidden="true"></i> Remove Friend</span>
-</div> &nbsp;
-<?php } } } $sql->close(); }
-if(isset($none) && $none) {
-    echo "Oops! It seems you don't have any friends yet. Why not add a user to your friend list?";
-} ?>
+            // if($recipient_lastlogin>0) {
+            //     $none = false;        
+            $sql2 = $conn->prepare("SELECT MAX(`timestamp`) FROM `logins` WHERE `userid`=?");
+            if(
+                $sql2 &&
+                $sql2->bind_param("i",$recipient_id) &&
+                $sql2->execute() &&
+                $sql2->store_result() &&
+                $sql2->bind_result($recipient_lastlogin)
+            ) {
+                $sql2->fetch();
+                ?>
+                <div class="mini-profile">
+                    <a href="./profile?u=<?php echo $recipient_id; ?>"><span class="mini-profile-name"><?php echo $recipient_name; ?>#<span class="mini-profile-tag"><?php echo $recipient_tag; ?></span></span></a><br/>
+                    <!-- <img src="./assets/avocado.png" class="mini-profile-img" /><br/> 
+                    <span class="mini-profile-high-score">High Score: 94</span><br/>-->
+                    <span class="mini-profile-registered">Registered <?php echo date("j M Y",$recipient_regdate); ?></span><br/>
+                    <span class="mini-profile-last-login">Last Active <?php echo date("j M Y",$recipient_lastlogin); ?></span><br/>
+                    <span class="mini-profile-remove-friend" id="remove-friend-<?php echo $recipient_id; ?>" onclick="removeFriend(<?php echo $recipient_id; ?>)"><i class="fas fa-user-minus" aria-hidden="true"></i> Remove Friend</span>
+                </div> &nbsp;
+                <?php $sql2->close();
+            } 
+        } 
+    } 
+    $sql->close(); 
+} 
+else {
+    echo $conn->error;
+}
+// if(isset($none) && $none) {
+//     echo "Oops! It seems you don't have any friends yet. Why not add a user to your friend list?";
+// } ?>
 
 <h2>Incoming Friend Requests</h2>
 <?php 
-$sql = $conn->prepare("SELECT DISTINCT `friend_requests`.`timestamp`,`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`,IFNULL(MAX(`logins`.`timestamp`),0)
+$sql = $conn->prepare("SELECT DISTINCT -- `friend_requests`.`timestamp`,
+`users`.`userid`,`users`.`usertag`,`users`.`username`,`users`.`registration_timestamp`
+-- ,IFNULL(MAX(`logins`.`timestamp`),0)
 FROM `friend_requests`
 LEFT JOIN `users` ON `friend_requests`.`sender_userid` = `users`.`userid`
-LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
+-- LEFT JOIN `logins` ON `users`.`userid` = `logins`.`userid`
 WHERE `friend_requests`.`recipient_userid`=? AND `friend_requests`.`status`=0 LIMIT 10");
 if( 
     $sql &&
     $sql->bind_param('i',$_SESSION["userid"]) &&
     $sql->execute() &&
     $sql->store_result() &&
-    $sql->bind_result($request_timestamp,$recipient_id,$recipient_tag,$recipient_name,$recipient_regdate,$recipient_lastlogin)
+    $sql->bind_result(/*$request_timestamp,*/$recipient_id,$recipient_tag,$recipient_name,$recipient_regdate/*,$recipient_lastlogin*/)
 ) {
     if($sql->num_rows<1) {
         echo "You currently have no incoming friend requests.";
     }
     else {
-        $none = true;
+        // $none = true;
         while($sql->fetch()) {
-            if($recipient_lastlogin>0) {
-                $none = false;
-?>
-<div class="mini-profile">
-    <a href="./profile?u=<?php echo $recipient_id; ?>"><span class="mini-profile-name"><?php echo $recipient_name; ?>#<span class="mini-profile-tag"><?php echo $recipient_tag; ?></span></span></a><br/>
-    <!-- <img src="./assets/avocado.png" class="mini-profile-img" /><br/> 
-    <span class="mini-profile-high-score">High Score: 94</span><br/>-->
-    <span class="mini-profile-registered">Registered <?php echo date("j M Y",$recipient_regdate); ?></span><br/>
-    <span class="mini-profile-last-login">Last Active <?php echo date("j M Y",$recipient_lastlogin); ?></span><br/>
-    <span class="mini-profile-accept-friend" onclick="acceptFriend(<?php echo $recipient_id; ?>)" id="accept-friend-<?php echo $recipient_id; ?>"><i class="fas fa-check-circle" aria-hidden="true"></i> Accept</span> <span class="mini-profile-decline-friend" onclick="declineFriend(<?php echo $recipient_id; ?>)" id="decline-friend-<?php echo $recipient_id; ?>"><i class="fas fa-times-circle" aria-hidden="true"></i> Decline</span>
-</div> &nbsp;
-<?php } } } $sql->close(); } else {
+            // if($recipient_lastlogin>0) {
+                // $none = false;
+            $sql2 = $conn->prepare("SELECT MAX(`timestamp`) FROM `logins` WHERE `userid`=?");
+            if(
+                $sql2 &&
+                $sql2->bind_param("i",$recipient_id) &&
+                $sql2->execute() &&
+                $sql2->store_result() &&
+                $sql2->bind_result($recipient_lastlogin)
+            ) {
+                $sql2->fetch();
+                ?>
+                <div class="mini-profile">
+                    <a href="./profile?u=<?php echo $recipient_id; ?>"><span class="mini-profile-name"><?php echo $recipient_name; ?>#<span class="mini-profile-tag"><?php echo $recipient_tag; ?></span></span></a><br/>
+                    <!-- <img src="./assets/avocado.png" class="mini-profile-img" /><br/> 
+                    <span class="mini-profile-high-score">High Score: 94</span><br/>-->
+                    <span class="mini-profile-registered">Registered <?php echo date("j M Y",$recipient_regdate); ?></span><br/>
+                    <span class="mini-profile-last-login">Last Active <?php echo date("j M Y",$recipient_lastlogin); ?></span><br/>
+                    <span class="mini-profile-accept-friend" onclick="acceptFriend(<?php echo $recipient_id; ?>)" id="accept-friend-<?php echo $recipient_id; ?>"><i class="fas fa-check-circle" aria-hidden="true"></i> Accept</span> <span class="mini-profile-decline-friend" onclick="declineFriend(<?php echo $recipient_id; ?>)" id="decline-friend-<?php echo $recipient_id; ?>"><i class="fas fa-times-circle" aria-hidden="true"></i> Decline</span>
+                </div> &nbsp;
+                <?php $sql2->close();
+            }   
+        } 
+    } 
+    $sql->close(); 
+} 
+else {
     echo $conn->error;
 } 
-if(isset($none) && $none) {
-    echo "You currently have no incoming friend requests.";
-} ?>
+// if(isset($none) && $none) {
+//     echo "You currently have no incoming friend requests.";
+// } ?>
 
 <h2>Outgoing Friend Requests</h2>
 <?php 
